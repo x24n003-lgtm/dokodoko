263423e1b4fba7d2b6b0a9c5db46c88d7f73ec75
Altimate-sakwa <x24n010@mitsuhashi.ac.jp>
2025-09-12 10:54:23 +0900

    tintin

--- test.php
+++ test.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>卒研EX</title>
 </head>
 <body>
     
