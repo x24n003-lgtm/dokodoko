859006af050759fb68d358de20e7f24f454ec77b
soramameTea <x24n009@mitsuhashi.ac.jp>
2025-09-19 10:19:09 +0900

    disney

--- mysql.php
+++ mysql.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>卒研</title>
+    <title>sqlとリンク</title>
 </head>
 <body>
-    
+    $sql = "SELECT * FROM `家計簿`;";
 </body>
 </html>
