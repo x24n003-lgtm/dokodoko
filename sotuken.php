06727d8a0b2267be180f5a7b0e4547babda9f3bd
Altimate-sakwa <x24n010@mitsuhashi.ac.jp>
2025-09-22 11:01:08 +0900

    php書いた

--- sotuken.php
+++ sotuken.php
@@ -1,9 +1,36 @@
+<?php
+$supabase_url = "https://ndufiudrmiyamcolrbjw.supabase.co/rest/v1/students";
+$service_role_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1ODM0MjQ3MiwiZXhwIjoyMDczOTE4NDcyfQ.62L7Sm1cNZXyFWMvtBw0z-9rZ1MPMylrvm797OEn7WE";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $supabase_url . "?select=*");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "Authorization: Bearer $service_role_key",
+    "Content-Type: application/json"
+]);
+
+$response = curl_exec($ch);
+
+if($response === false){
+    echo "cURL Error: " . curl_error($ch);
+} else {
+    $data = json_decode($response, true);
+    print_r($data);
+}
+
+curl_close($ch);
+?>
+
+
 <!DOCTYPE html>
+
 <html lang="ja">
 <head>
 <meta charset="UTF-8">
 <title>Leafletで現在地</title>
 <link rel="stylesheet" href="sotuken.css">
+
 
 <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
 <style>
@@ -31,6 +58,7 @@ function getLocation() {
     output.textContent = "このブラウザでは位置情報はサポートされていません";
     return;
   }
+  output.textContent = "位置情報を取得しています...";
 
   navigator.geolocation.getCurrentPosition(
     pos => {
