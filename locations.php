f661fd200c95d521e9e759f59a344fa80c2a9385
Altimate-sakwa <x24n010@mitsuhashi.ac.jp>
2025-09-26 10:29:38 +0900

    db追加

---
+++ locations.php
@@ -0,0 +1,25 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "sotuken";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+if ($conn->connect_error) {
+    die("接続失敗: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM places";
+$result = $conn->query($sql);
+
+$locations = [];
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $locations[] = $row;
+    }
+}
+
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($locations, JSON_UNESCAPED_UNICODE);
+
+$conn->close();
