ccc25ace13b8bc24574742d656af1cead7713bf6
Altimate-sakwa <x24n010@mitsuhashi.ac.jp>
2025-10-24 09:36:40 +0900

    SASUGANI

--- DBokuri.php
+++ DBokuri.php
@@ -1,11 +1,11 @@
 <?php
 // 1. DB接続
-$host = "172.16.199.21";  // Linux MariaDB の IP
-$user = "x24n007";
-$pass = "n051211";
-$db   = "dokodoko";
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "ドコどこ";
 
-$conn = new mysqli($host,$user,$pass,$db);
+$conn = new mysqli($servername,$username,$password,$dbname);
 
 // 接続チェック
 if ($conn->connect_error) {
